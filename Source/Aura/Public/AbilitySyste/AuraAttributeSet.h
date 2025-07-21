fb1c869ee9a6f6fcbcaa83207ac1861ad0f134d7
omar switi <omarswiti10@gmail.com>
2025-07-21 16:21:58 +0300

    Attributes

--- Source/Aura/Public/AbilitySyste/AuraAttributeSet.h
+++ Source/Aura/Public/AbilitySyste/AuraAttributeSet.h
@@ -4,8 +4,14 @@
 
 #include "CoreMinimal.h"
 #include "AttributeSet.h"
+#include  "AbilitySystemComponent.h"
 #include "AuraAttributeSet.generated.h"
 
+#define ATTRIBUTE_ACCESSORS_BASIC(ClassName, PropertyName) \
+	GAMEPLAYATTRIBUTE_PROPERTY_GETTER(ClassName, PropertyName) \
+	GAMEPLAYATTRIBUTE_VALUE_GETTER(PropertyName) \
+	GAMEPLAYATTRIBUTE_VALUE_SETTER(PropertyName) \
+	GAMEPLAYATTRIBUTE_VALUE_INITTER(PropertyName)
 /**
  * 
  */
@@ -13,5 +19,39 @@ UCLASS()
 class AURA_API UAuraAttributeSet : public UAttributeSet
 {
 	GENERATED_BODY()
+
+public:
+	UAuraAttributeSet();
+	virtual void GetLifetimeReplicatedProps(TArray<class FLifetimeProperty>& OutLifetimeProps) const override;
+
+	//this is called Boiler Plates
+	UPROPERTY(BlueprintReadOnly,Category="Vital Aattributes",ReplicatedUsing= OnRep_Health)
+	FGameplayAttributeData Health;
+	ATTRIBUTE_ACCESSORS_BASIC(UAuraAttributeSet,Health);
+	
+
+	UPROPERTY(BlueprintReadOnly,Category="Vital Aattributes",ReplicatedUsing= OnRep_MaxHealth)
+	FGameplayAttributeData MaxHealth;
+	ATTRIBUTE_ACCESSORS_BASIC(UAuraAttributeSet,MaxHealth);
+	
+	UPROPERTY(BlueprintReadOnly,Category="Vital Aattributes",ReplicatedUsing= OnRep_Mana)
+	FGameplayAttributeData Mana;
+	ATTRIBUTE_ACCESSORS_BASIC(UAuraAttributeSet,Mana);
+	
+	UPROPERTY(BlueprintReadOnly,Category="Vital Aattributes",ReplicatedUsing= OnRep_MaxMana)
+	FGameplayAttributeData MaxMana;
+	ATTRIBUTE_ACCESSORS_BASIC(UAuraAttributeSet,MaxMana);
+
+	UFUNCTION()
+	void OnRep_Health(const FGameplayAttributeData& OldHealth) const;
+
+	UFUNCTION()
+	void OnRep_MaxHealth(const FGameplayAttributeData& OldMaxHealth) const;
+
+	UFUNCTION()
+	void OnRep_Mana(const FGameplayAttributeData& OldMana) const;
+
+	UFUNCTION()
+	void OnRep_MaxMana(const FGameplayAttributeData& OldMaxMana) const;
 	
 };
