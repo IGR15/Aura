f0077e7baa5977fbf44502ac55134818f5380356
omar switi <omarswiti10@gmail.com>
2025-08-27 23:30:01 +0300

    Shaman and dead key

--- Source/Aura/Private/Actor/AuraProjectile.cpp
+++ Source/Aura/Private/Actor/AuraProjectile.cpp
@@ -60,7 +60,7 @@ void AAuraProjectile::Destroyed()
 void AAuraProjectile::OnsphereOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor,
                                       UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
 {
-	if (DamageEffectSpecHandle.Data.IsValid()&&DamageEffectSpecHandle.Data.Get()->GetContext().GetEffectCauser()==OtherActor)
+	if (!DamageEffectSpecHandle.Data.IsValid() || DamageEffectSpecHandle.Data.Get()->GetContext().GetEffectCauser()==OtherActor)
 	{
 		return;
 	}
