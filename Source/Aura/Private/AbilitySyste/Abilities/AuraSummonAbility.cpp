64074badfc8111f8005904e0b4c47cceb3372fb4
omar switi <omarswiti10@gmail.com>
2025-08-31 13:58:39 +0300

    Making algorithm

--- Source/Aura/Private/AbilitySyste/Abilities/AuraSummonAbility.cpp
+++ Source/Aura/Private/AbilitySyste/Abilities/AuraSummonAbility.cpp
@@ -9,28 +9,28 @@ TArray<FVector> UAuraSummonAbility::GetSpawnLocations()
 {
 	const FVector Forward=GetAvatarActorFromActorInfo()->GetActorForwardVector();
 	const FVector Location=GetAvatarActorFromActorInfo()->GetActorLocation();
-
-	const FVector LeftOfSpread=Forward.RotateAngleAxis(SpawnSpread/2.f,FVector::UpVector);
-
-	UKismetSystemLibrary::DrawDebugArrow(GetAvatarActorFromActorInfo(),
+	const float DeltaSpread=SpawnSpread/NumMinions;
+	const FVector RightOfSpread=Forward.RotateAngleAxis(SpawnSpread/2.f,FVector::UpVector);
+	const FVector LeftOfSpread=Forward.RotateAngleAxis(-SpawnSpread/2.f,FVector::UpVector);
+	TArray<FVector> SpawnLocations;
+	for (int i = 0; i < NumMinions; i++)
+	{
+		const FVector Direction=LeftOfSpread.RotateAngleAxis(DeltaSpread*i,FVector::UpVector);
+		const FVector ChosenLocation=Location+Direction*FMath::FRandRange(MinSpawnDistance,MaxSpawnDistance);
+		SpawnLocations.Add(ChosenLocation);
+		DrawDebugSphere(GetWorld(),ChosenLocation,20.f,12,FColor::Cyan,false,3.f);
+		UKismetSystemLibrary::DrawDebugArrow(GetAvatarActorFromActorInfo(),
 		Location,
-		Location+LeftOfSpread*MaxSpawnDistance,
+		Location+Direction*MaxSpawnDistance,
 		4.f,
 		FLinearColor::Green,
 		3.f
 
 		);
+		DrawDebugSphere(GetWorld(),Location+Direction*MinSpawnDistance,10.f,12,FColor::Red,false,3.f);
+		DrawDebugSphere(GetWorld(),Location+Direction*MaxSpawnDistance,10.f,12,FColor::Red,false,3.f);
 
-	const FVector RightOfSpread=Forward.RotateAngleAxis(-SpawnSpread/2.f,FVector::UpVector);
+	}
 
-	UKismetSystemLibrary::DrawDebugArrow(GetAvatarActorFromActorInfo(),
-		Location,
-		Location+RightOfSpread*MaxSpawnDistance,
-		4.f,
-		FLinearColor::Blue,
-		3.f
-
-		);
-
-	return TArray<FVector>();
+	return SpawnLocations;
 }
